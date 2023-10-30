10c7da88c2d167b158655fcd49078380200ced79
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-10-30 13:37:07 +0100

    Use methodshub.py

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, request, render_template
 
+from methodshub import MethodsHubContent
+
 app = Flask(__name__)
+
 
 @app.get("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 @app.post("/")
 def build():
-    print(request.form['git_repository_url'])
+    app.logger.info("Form content is %s", request.form)
+
+    methods_hub_content = MethodsHubContent(
+        request.form["git_repository_url"], request.form["filename"]
+    )
+
     return "<p>Hello, World!</p>"
