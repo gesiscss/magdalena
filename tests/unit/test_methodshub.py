b97cd97df24e90efa4c3d596a740b6a6b0ea3db0
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-10-30 17:19:24 +0100

    Fix PyTest cases

--- tests/unit/test_methodshub.py
+++ tests/unit/test_methodshub.py
@@ -66,7 +66,7 @@ class TestMethodsHubContent:
         assert methods_hub_content.domain == "gitlab.com"
         assert methods_hub_content.user_name == "lorem"
         assert methods_hub_content.repository_name == "ipsum"
-        assert methods_hub_content.tmp_path == "_github.com/lorem/ipsum"
+        assert methods_hub_content.tmp_path == "_gitlab.com/lorem/ipsum"
         assert methods_hub_content.filename_extension == "md"
 
     def test_init_with_gitlab_git(self):
@@ -81,25 +81,25 @@ class TestMethodsHubContent:
         assert methods_hub_content.domain == "gitlab.com"
         assert methods_hub_content.user_name == "lorem"
         assert methods_hub_content.repository_name == "ipsum"
-        assert methods_hub_content.tmp_path == "_github.com/lorem/ipsum"
+        assert methods_hub_content.tmp_path == "_gitlab.com/lorem/ipsum"
         assert methods_hub_content.filename_extension == "md"
 
     def test_init_filename_extension_txt(self):
-        with pytest.raises(ValueError):
-            assert methodshub.methodshub.MethodsHubContent(
-                "https://gitlab.com/lorem/ipsum.git", "lorem-ipsum.txt"
+        with pytest.raises(AssertionError):
+            assert methodshub.MethodsHubContent(
+                "https://github.com/lorem/ipsum.git", "lorem-ipsum.txt"
             )
 
     def test_init_filename_extension_md(self):
         methods_hub_content = methodshub.MethodsHubContent(
-            "https://gitlab.com/lorem/ipsum.git", "lorem-ipsum.md"
+            "https://github.com/lorem/ipsum.git", "lorem-ipsum.md"
         )
         assert (
             methods_hub_content.git_repository_url
-            == "https://gitlab.com/lorem/ipsum.git"
+            == "https://github.com/lorem/ipsum.git"
         )
         assert methods_hub_content.filename == "lorem-ipsum.md"
-        assert methods_hub_content.domain == "gitlab.com"
+        assert methods_hub_content.domain == "github.com"
         assert methods_hub_content.user_name == "lorem"
         assert methods_hub_content.repository_name == "ipsum"
         assert methods_hub_content.tmp_path == "_github.com/lorem/ipsum"
@@ -107,14 +107,14 @@ class TestMethodsHubContent:
 
     def test_init_filename_extension_qmd(self):
         methods_hub_content = methodshub.MethodsHubContent(
-            "https://gitlab.com/lorem/ipsum.git", "lorem-ipsum.qmd"
+            "https://github.com/lorem/ipsum.git", "lorem-ipsum.qmd"
         )
         assert (
             methods_hub_content.git_repository_url
-            == "https://gitlab.com/lorem/ipsum.git"
+            == "https://github.com/lorem/ipsum.git"
         )
         assert methods_hub_content.filename == "lorem-ipsum.qmd"
-        assert methods_hub_content.domain == "gitlab.com"
+        assert methods_hub_content.domain == "github.com"
         assert methods_hub_content.user_name == "lorem"
         assert methods_hub_content.repository_name == "ipsum"
         assert methods_hub_content.tmp_path == "_github.com/lorem/ipsum"
