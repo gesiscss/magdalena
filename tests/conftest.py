7ca857b4e7cc48e3ad7c6caf13fe9f3ff17b9ef4
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-11-08 09:12:57 +0100

    Add pytest conftest

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+
+from .. import app as magdalena
+
+@pytest.fixture()
+def app():
+    app = magdalena.app
+    app.config.update({
+        "TESTING": True,
+    })
+    yield 
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
