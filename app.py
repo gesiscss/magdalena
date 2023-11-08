3be8b9c188cb8bd4210016b1849782afd48063a5
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-11-08 17:39:37 +0100

    Complete render implementation

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask, request, render_template
+import os
+import shutil
+
+from flask import Flask, request, render_template, send_file
 
 from .methodshub import MethodsHubContent
 
 app = Flask(__name__)
+
+with app.app_context():
+    if "MAGDALENA_SHARED_DIR" not in os.environ:
+        app.logger.warn("MAGDALENA_SHARED_DIR is not defined! Using default.")
+        os.putenv("MAGDALENA_SHARED_DIR", "/tmp/magdalena-shared-volume")
+    shared_root_dir = os.getenv("MAGDALENA_SHARED_DIR")
+    app.logger.info("Shared directory is %s", shared_root_dir)
+    for dir_name in ("docker-scripts", "pandoc-filters"):
+        app.logger.info("Copying %s to %s", dir_name, shared_root_dir)
+        shutil.copytree(
+            dir_name, os.path.join(shared_root_dir, dir_name), dirs_exist_ok=True
+        )
 
 
 @app.get("/")
@@ -20,5 +35,8 @@ def build():
     assert methods_hub_content.clone_or_pull(), "Fail on clone or pull"
     assert methods_hub_content.create_container(), "Fail on container creation"
     assert methods_hub_content.render_all_formats(), "Fail on render contributions"
+    assert methods_hub_content.zip_all_formats(), "Fail on zip formats"
 
-    return "<p>Hello, World!</p>"
+    return send_file(
+        methods_hub_content.zip_filename, mimetype="application/zip", as_attachment=True
+    )
--- methodshub.py
+++ methodshub.py
@@ -1,7 +1,9 @@
 import logging
+import os
 import os.path
 import re
 import subprocess
+from zipfile import ZipFile 
 
 import docker
 import repo2docker
@@ -48,6 +50,19 @@ class MethodsHubContent:
 
         self.docker_repository = None
         self.docker_image_name = None
+        self.docker_user_name = "magdalena"
+        self.docker_user_id = 1000  # This is the first user in Ubuntu
+
+        self.docker_shared_dir = os.getenv("MAGDALENA_SHARED_DIR")
+        assert self.docker_shared_dir is not None
+
+        self.root_dir = os.path.dirname(os.path.abspath(__file__))
+        self.home_dir_at_docker = "/home/magdalena"
+        self.render_at_dir = None
+        self.output_location = f"{self.docker_shared_dir}/{self.domain}/{self.user_name}/{self.repository_name}/{self.filename}"
+        os.makedirs(self.output_location, exist_ok=True)
+
+        self.zip_filename = f"{self.domain}-{self.user_name}-{self.repository_name}-{self.filename}.zip"
 
     def clone_or_pull(self):
         if os.path.exists(self.tmp_path):
@@ -84,6 +99,7 @@ class MethodsHubContent:
             ), "Can NOT create Docker container if Git commit ID is None"
 
         self.docker_image_name = f"{self.docker_repository}:{self.git_commit_id}"
+        logger.info("Defined Docker image name: %s", self.docker_image_name)
 
         # Check if container already exists
         docker_client = docker.from_env()
@@ -98,8 +114,8 @@ class MethodsHubContent:
         r2d.log_level = logging.DEBUG
         r2d.run = False
         r2d.push = False
-        r2d.user_name = "magdalena"
-        r2d.user_id = 1000  # This is the first user in Ubuntu
+        r2d.user_name = self.docker_user_name
+        r2d.user_id = self.docker_user_id
         r2d.base_image = "gesiscss/repo2docker_base_image_with_quarto:v1.4.330"
         r2d.repo = self.git_repository_url
         # assert self.docker_image_name == 'magdalena/github.com/GESIS-Methods-Hub/minimal-example-ipynb-python:93a3b377f042298a65811a17356e25f30b276456'
@@ -116,32 +132,63 @@ class MethodsHubContent:
 
     def render_all_formats(self):
         RENDER_MATRIX = {
-            "md": (
-                "md2md.sh",
-                "md2qmd.sh",
-                "md2ipynb.sh"
-            ),
-            "qmd": (
-                "qmd2md.sh",
-                "qmd2qmd.sh",
-                "qmd2ipynb.sh"
-            ),
-            "Rmd": (
-                "Rmd2md.sh",
-                "Rmd2qmd.sh",
-                "Rmd2ipynb.sh"
-            ),
-            "ipynb": (
-                "ipynb2md.sh",
-                "ipynb2qmd.sh",
-                "ipynb2ipynb.sh"
-            ),
-            "docx": (
-                "docx2md.sh"
-            )
+            "md": ("md2html.sh", "md2qmd.sh", "md2ipynb.sh"),
+            "qmd": ("qmd2html.sh", "qmd2qmd.sh", "qmd2ipynb.sh"),
+            "Rmd": ("Rmd2html.sh", "Rmd2qmd.sh", "Rmd2ipynb.sh"),
+            "ipynb": ("ipynb2html.sh", "ipynb2qmd.sh", "ipynb2ipynb.sh"),
+            "docx": ("docx2html.sh"),
         }
 
         if self.filename_extension not in RENDER_MATRIX:
             raise ValueError("File extension not supported!")
+
+        docker_scripts_location = os.path.join(self.docker_shared_dir, "docker-scripts")
+        pandoc_filters_location = os.path.join(self.docker_shared_dir, "pandoc-filters")
+        output_location_in_container = self.docker_shared_dir
+
+        logger.info("Location of docker_scripts directory: %s", docker_scripts_location)
+        logger.info("Location of pandoc_filters directory: %s", pandoc_filters_location)
+        logger.info("Location of output directory: %s", self.output_location)
+        logger.info(
+            "Location of output directory in the sibling container: %s",
+            output_location_in_container,
+        )
+
+        host_user_id = os.getuid()
+        host_group_id = os.getgid()
+        mount_input_file = ""
+
+        for script in RENDER_MATRIX[self.filename_extension]:
+            if not os.path.isfile(os.path.join(docker_scripts_location, script)):
+                logger.error("Script %s not found! Skipping execution.", script)
+                continue
+
+            logger.info("Running %s ...", script)
+            render_content_subprocess = subprocess.run(
+                f"""docker run \\
+--user={host_user_id}:{host_group_id} \\
+{mount_input_file} \\
+--mount type=bind,source={docker_scripts_location},target={self.home_dir_at_docker}/_docker-scripts \\
+--env docker_script_root={self.home_dir_at_docker}/_docker-scripts \\
+--mount type=bind,source={pandoc_filters_location},target={self.home_dir_at_docker}/_pandoc-filters \\
+--mount type=bind,source={self.output_location},target={output_location_in_container} \\
+--env github_https={self.http_to_git_repository} \\
+--env github_user_name={self.user_name} \\
+--env github_repository_name={self.repository_name} \\
+--env file2render={self.filename} \\
+--env docker_image={self.docker_image_name} \\
+--env output_location={output_location_in_container} \\
+{self.docker_image_name} \\
+/bin/bash -c '{self.home_dir_at_docker}/_docker-scripts/{script}'""",
+capture_output=True, shell=True
+            )
+
+            assert render_content_subprocess.returncode == 0, "Fail to render content"
+
+        return True
+
+    def zip_all_formats(self):
+        with ZipFile(self.zip_filename, 'w') as zip_with_all_formats:
+            zip_with_all_formats.write(os.path.join(self.output_location, 'index.html'))
 
         return True
