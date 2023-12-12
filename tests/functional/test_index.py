d324460562d09035cba0208d4ab7232aa96650d4
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-12-12 09:57:33 +0100

    Add more functional tests

--- tests/functional/test_index.py
+++ tests/functional/test_index.py
@@ -7,14 +7,3 @@ def test_get_index(client):
 
     assert response_html.find(id="source_url") is not None
     assert response_html.find(id="filename") is not None
-
-
-def test_post_index_ipynb(client):
-    response = client.post(
-        "/",
-        data={
-            "source_url": "https://github.com/GESIS-Methods-Hub/minimal-example-ipynb-python",
-            "filename": "index.ipynb",
-        },
-    )
-    response_html = BeautifulSoup(response.data, "html.parser")
