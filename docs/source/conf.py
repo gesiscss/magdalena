214e652e56524fd915875ef4059d8dd39ee986f5
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2024-03-15 12:27:00 +0100

    Update documentation configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,11 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx_copybutton",
+    "sphinxcontrib.mermaid",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +28,26 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"  # Documentation at https://sphinx-rtd-theme.readthedocs.io/en/stable/index.html
+
+html_baseurl = '/magdalena/'
+
+html_copy_source = False
+
+html_theme_options = {
+    "display_version": True,
+    "prev_next_buttons_location": "both",
+    "style_external_links": True,
+    "vcs_pageview_mode": "blob",
+}
+
+html_context = {
+    "display_gitlab": True,
+    "gitlab_host": "git.gesis.org",
+    "gitlab_user": "methods-hub",
+    "gitlab_repo": "magdalena",
+    "gitlab_version": "main",
+    "conf_py_path": "/docs/source/",
+}
+
 html_static_path = ['_static']
