980c502b996a149d05f29d674e411d8211daa93a
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2024-03-18 11:47:52 +0100

    Add GraphQL mutation

--- magdalena/app.py
+++ magdalena/app.py
@@ -39,11 +39,22 @@ def build():
     ):
         assert "filename" in request.json, "Field filename missing in form"
         methods_hub_content = MethodsHubGitContent(
-            request.json["source_url"], request.json["filename"]
+            request.json["source_url"],
+            request.json["filename"],
+            (
+                request.json["forward_id"]
+                if ("forward_id" in request.json and len(request.json["forward_id"]))
+                else None
+            ),
         )
     else:
         methods_hub_content = MethodsHubHTTPContent(
             request.json["source_url"],
+            (
+                request.json["forward_id"]
+                if ("forward_id" in request.json and len(request.json["forward_id"]))
+                else None
+            ),
             (
                 request.json["filename"]
                 if ("filename" in request.json and len(request.json["filename"]))
@@ -57,17 +68,31 @@ def build():
         methods_hub_content.render_formats(request.json["target_format"]) is None
     ), "Fail on render contributions"
 
-    if len(request.json["target_format"]) == 1:
-        return send_file(
-            methods_hub_content.rendered_file(request.json["target_format"][0]),
-            mimetype="text/plain",
-            as_attachment=True,
-        )
-    else:
-        assert methods_hub_content.zip_all_formats() is None, "Fail on zip formats"
+    if request.json["response"] == "download":
+        app.logger.info("Sending response to user")
+        if len(request.json["target_format"]) == 1:
+            return send_file(
+                methods_hub_content.rendered_file(request.json["target_format"][0]),
+                mimetype="text/plain",
+                as_attachment=True,
+            )
+        else:
+            assert methods_hub_content.zip_all_formats() is None, "Fail on zip formats"
 
-        return send_file(
-            methods_hub_content.zip_file_path,
-            mimetype="application/zip",
-            as_attachment=True,
+            return send_file(
+                methods_hub_content.zip_file_path,
+                mimetype="application/zip",
+                as_attachment=True,
+            )
+
+    if request.json["response"] == "forward":
+        app.logger.info(
+            "Sending response to %s", os.getenv("MAGDALENA_GRAPHQL_TARGET_URL")
         )
+
+        if len(request.json["target_format"]) == 1:
+            methods_hub_content.push_rendered_formats(request.json["target_format"])
+        else:
+            methods_hub_content.push_all_rendered_formats()
+
+        return {"status": "OK"}
