b2dd6552f5f1a0d1f84c7c0bb9f45503885e63bd
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2024-03-20 09:29:23 +0100

    WIP

--- magdalena/tests/functional/test_api.py
+++ magdalena/tests/functional/test_api.py
@@ -1,13 +1,24 @@
+from lxml import etree
+from lxml.cssselect import CSSSelector
+
+
 def test_post_ipynb_to_md(client):
     response = client.post(
         "/",
         json={
             "source_url": "https://github.com/GESIS-Methods-Hub/minimal-example-ipynb-python",
             "filename": "index.ipynb",
-            "target_format": ["md"],
+            "target_format": ["html"],
             "response": "download",
         },
     )
+
+    response_html = etree.fromstring(response.data, etree.HTMLParser())
+
+    selector = CSSSelector("main")
+    selection = selector(response_html)
+
+    assert len(selection) > 0, "HTML document does NOT have <main>!"
 
 
 def test_post_docx_to_md(client):
@@ -15,22 +26,17 @@ def test_post_docx_to_md(client):
         "/",
         json={
             "source_url": "https://gesisbox.gesis.org/index.php/s/tPbi9bXiHfXAHR4",
-            "target_format": ["md"],
+            "target_format": ["html"],
             "response": "download",
         },
     )
 
-    expected_markdown = b"""---
-citation: true
-guide: true
-info_pandoc_version: 3.1.2
-keywords: {}
-source_filename: lorem-ipsum.docx
----
+    response_html = etree.fromstring(response.data, etree.HTMLParser())
 
-Lorem ipsum.
-"""
-    assert response.data == expected_markdown
+    selector = CSSSelector("main")
+    selection = selector(response_html)
+
+    assert len(selection) > 0, "HTML document does NOT have <main>!"
 
 
 def test_post_docx_to_md_with_filename(client):
@@ -39,19 +45,14 @@ def test_post_docx_to_md_with_filename(client):
         json={
             "source_url": "https://gesisbox.gesis.org/index.php/s/tPbi9bXiHfXAHR4",
             "filename": "minimal.docx",
-            "target_format": ["md"],
+            "target_format": ["html"],
             "response": "download",
         },
     )
 
-    expected_markdown = b"""---
-citation: true
-guide: true
-info_pandoc_version: 3.1.2
-keywords: {}
-source_filename: minimal.docx
----
+    response_html = etree.fromstring(response.data, etree.HTMLParser())
 
-Lorem ipsum.
-"""
-    assert response.data == expected_markdown
+    selector = CSSSelector("main")
+    selection = selector(response_html)
+
+    assert len(selection) > 0, "HTML document does NOT have <main>!"
