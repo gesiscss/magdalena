c3a9658ec783fd70ff30c2b8f71f1800f775cc76
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2024-03-27 15:08:08 +0100

    Keep public key in memory

--- magdalena/pem.py
+++ magdalena/pem.py
@@ -12,9 +12,7 @@ assert KEYCLOAK_REALM is not None, "KEYCLOAK_REALM can't be None"
 
 KEYCLOAK_ISSUER = f"{KEYCLOAK_SCHEME}://{KEYCLOAK_DOMAIN}/realms/{KEYCLOAK_REALM}"
 
-def create_public_key():
-    public_key_path = os.path.join(os.path.dirname(__file__), "public.key")
-
+def retrieve_public_key():
     request = requests.get(KEYCLOAK_ISSUER)
 
     assert request.status_code == 200, "Keycloak answer must be 200"
@@ -23,15 +21,14 @@ def create_public_key():
 
     public_key = request_as_json['public_key']
 
-    with open(public_key_path, "w") as pem_file:
-        # Documented at https://www.rfc-editor.org/rfc/rfc7468
-        pem_file.write("-----BEGIN RSA PUBLIC KEY-----\n")
-        # 16 groups of 4 chars, which means exactly 64 chars per; line, except the final line
-        for i in range(len(public_key) // 64):
-            begin = i * 64
-            end = (i + 1) * 64
-            pem_file.write(f"{public_key[begin:end]}\n")
-        pem_file.write(f"{public_key[end:]}\n")
-        pem_file.write("-----END RSA PUBLIC KEY-----\n")
+    # Documented at https://www.rfc-editor.org/rfc/rfc7468
+    pem_public_key = "-----BEGIN RSA PUBLIC KEY-----\n"
+    # 16 groups of 4 chars, which means exactly 64 chars per; line, except the final line
+    for i in range(len(public_key) // 64):
+        begin = i * 64
+        end = (i + 1) * 64
+        pem_public_key += f"{public_key[begin:end]}\n"
+    pem_public_key += f"{public_key[end:]}\n"
+    pem_public_key += "-----END RSA PUBLIC KEY-----\n"
 
-    return public_key_path
+    return pem_public_key
